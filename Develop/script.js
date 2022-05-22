53ae6a357ba1e5d97680f8897424cb5538d290b2
Bradley Parris <parrisbjp@gmail.com>
2022-05-22 08:46:57 -0400

    asks user for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,37 @@
 // Assignment code here
 
+// Get password length
+var passwordLength
+
+function promptLength() {
+  
+  var lengthInput = window.prompt("Please choose a password length between 8 and 128 characters.");
+
+  // If lengthInput is not a number then user is alerted and prompted again
+  if (isNaN(lengthInput)) {
+    window.alert("Please enter a numerical value.");
+    promptLength();
+  }
+
+  // If lengthInput isn't a number between 8 and 128, then user is alerted then prompted again
+  else if (lengthInput < 8 || lengthInput > 128) {
+    window.alert("Please enter a valid character length.");
+    promptLength();
+  }
+
+  // Gives passwordLength the value of the user's desired password length
+  else {
+    passwordLength = lengthInput;
+    console.log(passwordLength);
+  }
+}
+
+promptLength();
+
+// Generate Password function
+function generatePassword() {
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
