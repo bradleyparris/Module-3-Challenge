983993386333e2c0a7a1d4c3eb1ea400aec00bb1
Bradley Parris <parrisbjp@gmail.com>
2022-05-22 08:59:42 -0400

    asks if user wants uppercase letters in password

--- Develop/script.js
+++ Develop/script.js
@@ -3,30 +3,58 @@
 // Get password length
 var passwordLength
 
-function promptLength() {
-  
-  var lengthInput = window.prompt("Please choose a password length between 8 and 128 characters.");
+  function promptLength() {
+    
+    var lengthInput = window.prompt("Please choose a password length between 8 and 128 characters.");
 
-  // If lengthInput is not a number then user is alerted and prompted again
-  if (isNaN(lengthInput)) {
-    window.alert("Please enter a numerical value.");
-    promptLength();
+    // If lengthInput is not a number then user is alerted and prompted again
+    if (isNaN(lengthInput)) {
+      window.alert("Please enter a numerical value.");
+      promptLength();
+    }
+
+    // If lengthInput isn't a number between 8 and 128, then user is alerted then prompted again
+    else if (lengthInput < 8 || lengthInput > 128) {
+      window.alert("Please enter a valid character length.");
+      promptLength();
+    }
+
+    // Gives passwordLength the value of the user's desired password length
+    else {
+      passwordLength = lengthInput;
+      console.log(passwordLength);
+    }
   }
 
-  // If lengthInput isn't a number between 8 and 128, then user is alerted then prompted again
-  else if (lengthInput < 8 || lengthInput > 128) {
-    window.alert("Please enter a valid character length.");
-    promptLength();
+  promptLength();
+
+// Get uppercase or lowercase
+var passwordCase
+
+  // Prompt user
+  function promptCase() {
+    var caseInput = window.prompt("Would you like to include uppercase letters? (YES or NO)");
+    // Changes input to lower case to make it easier to process
+    caseInput = caseInput.toLowerCase();
+
+    // If user wants upper case letters in password
+    if (caseInput === "yes") {
+      passwordCase = true;
+      return passwordCase;
+    }
+    // If user doesn't want upper case letters in password
+    else if (caseInput === "no") {
+      passwordCase = false;
+      return passwordCase;
+    }
+    else {
+      window.alert("Please enter YES or NO");
+      promptCase();
+    }
+    console.log(passwordCase);
   }
 
-  // Gives passwordLength the value of the user's desired password length
-  else {
-    passwordLength = lengthInput;
-    console.log(passwordLength);
-  }
-}
-
-promptLength();
+  promptCase();
 
 // Generate Password function
 function generatePassword() {
